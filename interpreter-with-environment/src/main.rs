c31cea467726e752dc351082342b783f3734d529
Andrew Ferguson <aferguson@ltsp194.udallas.edu>
2022-05-03 07:51:20 -0500

    Updated with Mirabelli's code

--- interpreter-with-environment/src/main.rs
+++ interpreter-with-environment/src/main.rs
@@ -1,33 +1,32 @@
 use std::collections::HashMap;
 
-#[derive(Debug)]
-pub enum Expression {
+pub enum Expression{
     Add(Vec<Expression>),
     Multiply(Vec<Expression>),
     Subtract(Vec<Expression>),
     Variable(String),
-    Number(f64)
+    Number(f64),
 }
 
-pub struct Environment {
-     hash_map: HashMap<String, Expression>,
-     parent: Box<Option<Environment>>,
+pub struct Enviroment {
+    hash_map: HashMap<String, Expression>,
+    parent: Box<Option<Enviroment>>,
 }
 
-impl Environment {
-    fn new(parent: Environment) -> Environment {
-        Environment { hash_map: HashMap::new(), parent: Box::new(Option::Some(parent)) }
+impl Enviroment{
+    fn new(parent: Enviroment) -> Enviroment {
+        Enviroment{hash_map: HashMap::new(), parent: Box::new(Option::Some(parent))}
     }
 
-    fn root() -> Environment {
-        Environment{hash_map: HashMap::new(), parent: Box::new(Option::None)}
+    fn root() -> Enviroment {
+        Enviroment{hash_map: HashMap::new(), parent: Box::new(Option::None)}
     }
 
-    fn insert(self: &mut Environment, key: String, expression: Expression) {
+    fn insert(self: &mut Enviroment, key: String, expression: Expression) {
         self.hash_map.insert(key, expression);
-
     }
-    fn value_for_key(self: &Environment, key: &String) -> &Expression {
+
+    fn value_for_key(self: &Enviroment, key: &String) -> &Expression {
         match self.hash_map.get(key) {
             Some(expression) => expression,
             None => match &*self.parent {
@@ -36,192 +35,145 @@ impl Environment {
             }
         }
     }
-
-
 }
 
-fn evaluate(expression: &Expression, environment: &Environment) -> f64 {
-    match expression {
-        Expression::Number(value) => *value, 
-        Expression::Add(_) => evaluate_addition(expression, environment),
-        Expression::Multiply(_) => evaluate_multiplication(expression, environment),
-        Expression::Variable(name) => evaluate(&environment.value_for_key(&name), environment),
-        Expression::Subtract(_) => evaluate_subtraction(expression, environment)
+fn evaluate(expression: &Expression, enviroment: &Enviroment) -> f64{
+    match expression{
+        Expression::Number(value)=> *value, 
+        Expression::Add(_)=> evaluate_addition(expression, enviroment), 
+        Expression::Multiply(_)=> evaluate_multiplication(expression, enviroment),
+        Expression::Subtract(_) => evaluate_subtraction(expression, enviroment),
+        Expression::Variable(name)=> evaluate(&enviroment.value_for_key(&name), enviroment),
+        _=> -1.0
     }
 }
-
-fn evaluate_addition(add: &Expression, environment: &Environment) -> f64 {
+fn  evaluate_subtraction(add: &Expression, enviroment: &Enviroment) -> f64 {
     if let Expression::Add(expressions) = add {
-        let iter = expressions.iter();
-        iter.fold(0.0, |total, next| total + evaluate(next, environment))
-    } else {
-        panic!("expected addition")
-    }
-
-}
-
-fn evaluate_multiplication(multiply: &Expression, environment: &Environment) -> f64 {
-    if let Expression::Multiply(expressions) = multiply {
-        let iter = expressions.iter();
-        iter.fold(1.0, |total, next| total * evaluate(next, environment))
-    } else {
-        panic!("expected multiplication")
-    }
-}
-fn print_expression(expression: &Expression) {
-    match expression {
-        Expression::Add(expressions) => {
-            print!("(+");
-            for e in expressions {
-                print!(" ");
-                print_expression(e)
-            }
-            print!(")");
-        },
-        Expression::Multiply(expressions) => {
-            print!("(*");
-            for e in expressions {
-                print!(" ");
-                print_expression(e)
-            }
-            print!(")");
-        },
-        Expression::Subtract(expressions) => {
-            print!("(-");
-            for e in expressions {
-                print!(" ");
-                print_expression(e)
-            }
-            print!(")");
-        }
-        Expression::Variable(variable) => {
-            print!("{}",variable);
-        }
-        Expression::Number(val) => {print!("{}", val)}
-    }
-
-}
-
-fn evaluate_subtraction(subtract: &Expression, enviroment: &Environment) -> f64 {
-    if let Expression::Subtract(expressions) = subtract {
         let mut iter = expressions.iter();
         let start = iter.next();
-        if let Some(Expression::Number(val)) = start {
-            iter.fold(*val, |total, next| total - evaluate(next, enviroment))
+        if let Some(Expression::Number(value)) = start{
+            iter.fold(*value, |total, next| total - evaluate(next, enviroment))
         } else {
             0.0
         }
         
     } else {
-        panic!("expected subtraction")
+        panic!("expected subtraction");
+    }
+}
+fn  evaluate_addition(add: &Expression, enviroment: &Enviroment) -> f64 {
+    if let Expression::Add(expressions) = add {
+        let iter = expressions.iter();
+        iter.fold(0.0, |total, next| total + evaluate(next, enviroment))
+
+    } else {
+        panic!("expected addition");
     }
 }
 
-fn main() {
+fn evaluate_multiplication(multiply: &Expression, enviroment: &Enviroment )-> f64 {
+    if let Expression::Multiply(expressions) = multiply {
+        let iter = expressions.iter();
+        iter.fold(1.0, |total, next| total * evaluate(next, enviroment))
 
+    } else {
+        panic!("expected multiplication")
+    }
 }
 
-#[cfg(test)]
-mod testing {
-    use crate::HashMap;
-    use crate::Environment;
-    use crate::Expression;
-    use crate::evaluate;
+fn main() {}
 
-    fn num(value: f64) -> Expression{
+
+//configure testing modules
+
+#[cfg(test)]
+
+mod tests {
+
+    use crate::Expression;
+    use crate::Enviroment;
+
+    fn num(value: f64)-> crate::Expression{
         Expression::Number(value)
     }
 
     #[test]
-    fn test_bootstrap() {
-        assert_eq!(0,0)
+    fn test_bootstrap(){
+        assert_eq!(0, 0);
     }
-
     #[test]
-    fn test_evaluate_number() {
-        // arrange
-        let number = num(37.0);
-        // act
-        let value = crate::evaluate(&number, &crate::Environment::root());
+    fn test_evaluate_number(){
+        //three phases: arrange, act, assert
+        //arrange
+        let number = num(42.0);
+        //act
+        let value = crate::evaluate(&number, &crate::Enviroment::root());
         //assert
-        assert_eq!(value, 37.0)
+
+        assert_eq!(value, 42.0);
     }
 
-
+    #[test]
+    fn test_enviroment_contains_variable(){
+        //arrange
+        let mut enviroment = Enviroment::root();
+        enviroment.insert("foo".to_string(), num(5.0));
+        //act
+        let key = "foo".to_string(); 
+        let value = crate::evaluate(&Expression::Variable("foo".to_string()), &enviroment); 
+        //assert
+        assert_eq!(value, 5.0);
+    }
 
     #[test]
-    fn test_addition() {
+    fn test_addition(){
+        //arrange
         let addition = crate::Expression::Add(vec![num(2.0), num(2.0)]);
-        let enviroment = &crate::Environment::root();
 
-        let value = crate::evaluate(&addition, enviroment);
-
-        assert_eq!(value, 4.0)
+        //act
+        let value = crate::evaluate(&addition, &crate::Enviroment::root());
+        //assert
+        assert_eq!(value, 4.0); 
     }
+
     #[test]
-    fn test_variable() {
-        let mut environment = Environment::root();
+
+    fn test_multiplication(){
+        //arrange
+        let multiplication = crate::Expression::Multiply(vec![num(2.0), num(3.0)]);
+        //act
+        let value = crate::evaluate(&multiplication, &crate::Enviroment::root());
+        //assert
+        assert_eq!(value, 6.0);
+    }
+
+
+    #[test]
+    fn test_variable_in_addition(){
+        //define foo 5.0 (+2.0 foo)
+
+        let variable = crate::Expression::Variable(String::from("foo"));
+        let mut environment = Enviroment::root();
         environment.insert("foo".to_string(), num(5.0));
+        let expression = Expression::Add(vec![num (2.0), variable]); 
+        //act
+        let value = crate::evaluate(&expression, &environment);
 
-        let key = "foo".to_string();
-        let variable = Expression::Variable("foo".to_string());
-        let value = evaluate(&variable, &environment );
-
-        assert_eq!(value, 5.0)
-    }
-    #[test]
-    fn test_subtraction() { 
-        let subtraction = crate::Expression::Subtract(vec![num(2.0), num(2.0), num(2.0)]);
-        let enviroment = crate::Environment::root();
-
-        let value = crate::evaluate(&subtraction, &enviroment);
-
-        assert_eq!(value, -2.0)
-    }
-    #[test]
-    fn test_variable_in_addition() {
-        let variable = Expression::Variable(String::from("foo"));
-        let mut environment = Environment::root();
-        environment.insert("foo".to_string(), num(5.0));
-        let expression = Expression::Add(vec![num(2.0), variable]);
-        let value = evaluate(&expression, &environment);
-
-        assert_eq!(value, 7.0)
-
-
+        //assert
+        assert_eq!(value, 7.0);
     }
 
     #[test]
     fn test_multilevel_environment() {
         // arrange
-        let mut root = Environment::root();
+        let mut root = Enviroment::root();
         root.insert("foo".to_string(), num(42.0));
-        let mut sub = Environment::new(root);
+        let mut sub = Enviroment::new(root);
         sub.insert("bar".to_string(), num(5.0));
-
-        // act 
-        let should_be_42 = evaluate(&Expression::Variable("foo".to_string()), &sub);
-
-
-        // assert
-        assert_eq!(should_be_42, 42.0)
-
-    }
-    #[test]
-    fn test_symmetrical_variables() {
-        // arrange
-        let mut root = Environment::root();
-        root.insert("x".to_string(), num(42.0));
-        let mut sub = Environment::new(root);
-        sub.insert("x".to_string(), num(5.0));
-
         // act
-        let should_be_5 = evaluate(&Expression::Variable("x".to_string()), &sub);
-
+        let should_be_42 = crate::evaluate(&Expression::Variable("foo".to_string()), &sub);
         // assert
-        assert_eq!(should_be_5, 5.0)
-
-        
+        assert_eq!(should_be_42, 42.0);
     }
 
 }
--- interpreter-with-environment/target/rls/debug/deps/terp-9e699e4ea52aebf4.d
+++ interpreter-with-environment/target/rls/debug/deps/terp-9e699e4ea52aebf4.d
@@ -1,5 +1,5 @@
-/home/aferguson/Spring 2022/Programming Languages/interpreter/interpreter/target/rls/debug/deps/terp-9e699e4ea52aebf4.rmeta: src/main.rs
+/home/aferguson/Courses/Spring 2022/Programming Languages/interpreter/interpreter-with-environment/target/rls/debug/deps/terp-9e699e4ea52aebf4.rmeta: src/main.rs
 
-/home/aferguson/Spring 2022/Programming Languages/interpreter/interpreter/target/rls/debug/deps/terp-9e699e4ea52aebf4.d: src/main.rs
+/home/aferguson/Courses/Spring 2022/Programming Languages/interpreter/interpreter-with-environment/target/rls/debug/deps/terp-9e699e4ea52aebf4.d: src/main.rs
 
 src/main.rs:
